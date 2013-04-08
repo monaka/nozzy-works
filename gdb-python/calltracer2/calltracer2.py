17cd8032e381447304a5029bd03ce7431be0ec1a
Takahide Nojima <nozzy123nozzy@gmail.com>
2013-04-08 22:19:43 +0900

    update calltracer2

--- gdb-python/calltracer2/calltracer2.py
+++ gdb-python/calltracer2/calltracer2.py
@@ -1,20 +1,5 @@
+# -*- coding: utf-8 -*-
 import re
-
-class _MediatorForCallTracer(object):
-    """ mediator for updating state of calltracer """
-    def __init__(self):
-        self._signals = {}
-
-    def signal(self,signal_name,*args, **kw):
-        for handler in self._signals.get(signal_name,[]):
-            handler(*args, **kw)
-
-    def connect(self, signal_name, receiver):
-        handlers = self._signals.setdefault(signal_name,[])
-        handlers.append(receiver)
-
-    def disconnect(self, signal_name, receiver):
-        handlers[signal_name].remove(receiver)
 
 class _CallTracerFinishBreakpoint(gdb.FinishBreakpoint):
     def __init__(self, name, stack):
@@ -95,12 +80,12 @@ class _PrepareCallTracer(gdb.Command):
 _PrepareCallTracer()
 
 class _CallTracerLoadBinaryBreakpoint(gdb.Breakpoint):
-    def __init__(self, spec,binary_info,stack,most_recently_loaded):
+    def __init__(self, spec,binary_info,stack):
         super(_CallTracerLoadBinaryBreakpoint, self).__init__(spec, 
                                                     gdb.BP_BREAKPOINT,
                                                     internal = False)
         self.silent=True
-        self._most_recently_loaded=most_recently_loaded
+        self._last_loaded=""
         self._record_regrex=re.compile(r'^0x[^/]+(.+)$')
         self._binary_info=binary_info
         self._stack=stack
@@ -117,11 +102,12 @@ class _CallTracerLoadBinaryBreakpoint(gdb.Breakpoint):
 
     def stop(self):
         print "break in loader"
-        loaded_candidate=_retrive_loaded_binary()
-        if loaded_candidate != self._most_recently_loaded:
-            self._most_recently_loaded = loaded_candidate
-            if self._binary_info[self._most_recently_loaded]:
-                for func_spec in self._binary_info[self._most_recently_loaded]:
+        loaded_candidate=self._retrive_loaded_binary()
+        if loaded_candidate != self._last_loaded:
+            self._last_loaded = loaded_candidate
+            print "Loading: %s" % self._last_loaded
+            if self._binary_info.has_key(self._last_loaded) == True:
+                for func_spec in self._binary_info[self._last_loaded]:
                     _CallTracerBreakpoint(func_spec,func_spec,self._stack)
 
         return False
@@ -129,18 +115,53 @@ class _CallTracerLoadBinaryBreakpoint(gdb.Breakpoint):
 class _BreakSetupFromFile(gdb.Command):
     """ set up trace point from file """
     def __init__(self):
-        super(_BreakSetupFromFile, self).__init__('setuptracepoint',
+        super(_BreakSetupFromFile, self).__init__('setuptracepointfromfile',
                                                   gdb.COMMAND_OBSCURE)
         self._stack=[]
         self._most_recently_loaded_binary=""
 
     def _load_binary_lst(self,filename):
-        with open("binary.lst") as fh:
+        binary_trace_info={};
+        token_state=r'FindOutHeader'
+        binary_token_regrex=re.compile(r'^[Bb]inary:\s*([^\s]+)')
+        record_sep_regrex=re.compile(r'^\s*$')
+        with open(filename) as fh:
+            for ln in fh.readlines():
+                ln = ln.strip('\r\n')
+                if token_state == 'FindOutHeader':
+                    found=binary_token_regrex.search(ln)
+                    if found:
+                        library_name=found.group(1)
+                        if binary_trace_info.has_key('program_name') == False:
+                            binary_trace_info['program_name']=library_name
+                            print "setup program_name"
+
+                        binary_trace_info[library_name]=[];
+                        token_state='FindOutFuncs'
+                    continue
+                elif token_state == 'FindOutFuncs':
+                    found=record_sep_regrex.search(ln)
+                    if found:
+                        token_state='FindOutHeader'
+                        continue
+                    else:
+                        binary_trace_info[library_name].append(ln)
+
+        return binary_trace_info;
+
+    def _setup_program_breaks(self):
+        program_name=self._binary_trace_info['program_name']
+        for func_spec in self._binary_trace_info[program_name]:
+            _CallTracerBreakpoint(func_spec,func_spec,self._stack)
             
     def invoke(self, arg, from_tty):
-        self._binary_trace_info=_load_binary_lst("binary.lst")
+        self._binary_trace_info=self._load_binary_lst("binary.lst")
         # setup loader breaks
         gdb.execute("set pagination off")
+        _CallTracerLoadBinaryBreakpoint("_dl_debug_state",
+                                        self._binary_trace_info,
+                                        self._stack)
+        self._setup_program_breaks()        
         print "prepare from file done!"
 
 _BreakSetupFromFile()
