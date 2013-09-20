b8ff1bc33862af04afb34c5f7f10584aafc841e8
Takahide Nojima <nozzy123nozzy@gmail.com>
2013-09-21 03:24:48 +0900

    update prog

--- pixman-test/pixman-fb.c
+++ pixman-test/pixman-fb.c
@@ -10,7 +10,7 @@
 #include <unistd.h>
 #include <string.h>
 
-int main (int argc,char **argv)
+int setup_terminal(void)
 {
 	int tty0_fd,new_vt;
 	int new_tty_fd,kb_mode;
@@ -22,60 +22,79 @@ int main (int argc,char **argv)
 	tty0_fd=open("/dev/tty0",O_WRONLY | O_CLOEXEC);
 	if (tty0_fd < 0) {
 		perror("uh? cant open /dev/tty0,abort");
-		exit(1);
+		return 1;
 	}
 	if ( ioctl(tty0_fd,VT_OPENQRY,&new_vt) <0 ) {
 		perror("uh? cant query new vc,abort");
-		exit(1);
+		return 1;
 	}
 	if (new_vt <0) {
 		fprintf(stderr,"no more new vc,abort");
-		exit(1);
+		return 1;
 	}	
 	close(tty0_fd);
 	snprintf(fname,sizeof(fname),"/dev/tty%d",new_vt);
 	new_tty_fd=open(fname, O_RDWR | O_NOCTTY | O_CLOEXEC);
 	if (new_tty_fd <0) {
 		perror("uh? cant open new vc ,abort");
-		exit(1);
+		return 1;
 	}
 	if (ioctl(new_tty_fd, VT_GETSTATE, &vts) ==0) {
 		if (ioctl(new_tty_fd, VT_ACTIVATE, new_vt) <0) {
 			perror("uh? cant active new vc ,abort");
-			exit(1);
+			return 1;
 		}
 		if (ioctl(new_tty_fd, VT_WAITACTIVE, new_vt) <0) {
 			perror("uh? cant wait for activation of new vc ,abort");
-			exit(1);
+			return 1;
 		}
 	}
 	printf("Now activate %s ...\n",fname);
 	if (tcgetattr(new_tty_fd,&tty_attr) < 0 ) {
 		perror("uh? cant get attribute of tty,abort");
-		exit(1);
+		return 1;
 	}
 	cfmakeraw(&tty_attr);
 	tty_attr.c_oflag |= OPOST | OCRNL;
 	if (tcsetattr(new_tty_fd,TCSANOW, &tty_attr) < 0 ) {
 		perror("uh? cant set attribute of tty,abort");
-		exit(1);
+		return 1;
 	}
 	if (ioctl(new_tty_fd, KDSKBMODE,K_OFF)) {
 		if (ioctl(new_tty_fd, KDSKBMODE, K_RAW)) {
 			perror("uh? KBD is not set K_RAW,abort");
-			exit(1);
+			return 1;
 		}
 	}
 	if (ioctl(new_tty_fd, KDSKBMODE,KD_GRAPHICS)) {
 		perror("uh? KBD is not set KD_GRAPHICS,abort");
-		exit(1);
+		return 1;
 	}
+	return 0;
+}
+int mkframe_buffer(void)
+{
+	char *dev="/dev/fb0";
+	int frame_fd;
+	frame_fd=open(dev, O_RDWR | O_CLOEXEC);
+	if (frame_fd<0) {
+		perror("uh? cant open frame buffer,abort");
+		return 1;
+	}
+	
+}
+int main (int argc,char **argv)
+{
+	/* frame buffer create */
+	if (setup_terminal()) {
+		exit(1);
+	}	
 	while(1) {
 		memset(&tty_buf[0],0,sizeof(tty_buf));
 		n_char=read(new_tty_fd,&tty_buf[0], sizeof(tty_buf)-1);
 		if (n_char <0) {
 			perror("uh? failure to read from tty,abort");
-			exit(1);
+			return 1;
 		} else if  (n_char == 0) {
 			sleep(1); /* avoid busy loop */
 		}
